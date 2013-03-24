83b20e1990963273e52556818437be9dbbe72acb
Eugene Lin <eugenelin89@host204-237.wifi.ubc.ca>
2013-03-23 22:36:35 -0700

    refactor

--- Caribbean/DummyData.h
+++ Caribbean/DummyData.h
@@ -11,22 +11,22 @@
 
 #define DUMMY_TARGETS_JSON @"{\"data\" :\
 [\
-{\"id\": 1, \"title\":\"Free Pizza\", \"loc\":[49.216338, -122.981009]\
-,\"name\":\"Freshslice Pizza\"\
-,\"desc1\":\"Snap a picture of the sign at this location and receive\"\
-,\"desc2\":\"one slice of freshly baked pizza from Freshslice Pizza.\"\
+{\"id\": 1, \"title\":\"What a pizza!\", \"loc\":[49.216338, -122.981009]\
+,\"name\":\"Veggie Please\"\
+,\"desc1\":\"Order a slice of veggie pizza location and take\"\
+,\"desc2\":\"a picture of your significant others take a bite.\"\
 ,\"desc3\":\"\"\
 ,\"headerImg\":\"http://3.bp.blogspot.com/-bOBsMq_lnoo/Tw6TEMdUHkI/AAAAAAAAACU/gH2okJ6JzPY/s640/IMG_3281.JPG\"\
 ,\"brandImg\":\"http://www.clickmyslice.ca/logo/fresh_slice_pizza_1.png\"\
 ,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
 ,\"endDateTime\":\"2013-11-30T07:00:00.000Z\"\
-,\"points\":0\
+,\"points\":100\
 ,\"currentPlayers\":0\
 },\
-{\"id\": 2, \"title\":\"Free Pizza\", \"loc\":[49.283868, -123.111961]\
-,\"name\":\"Michael Angelos Mega Slice\"\
-,\"desc1\":\"Snap a picture of Michael Angelos himself,\"\
-,\"desc2\":\"Get a free slice of pizza from Michael Angelos!\"\
+{\"id\": 2, \"title\":\"Renaissance Man\", \"loc\":[49.283868, -123.111961]\
+,\"name\":\"The Hunt for Michael Angelos\"\
+,\"desc1\":\"Snap a picture with the amazing artist\"\
+,\"desc2\":\"\"\
 ,\"desc3\":\"\"\
 ,\"headerImg\":\"http://eugenicode.com/IMG_4373.JPG\"\
 ,\"brandImg\":\"https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-snc7/418949_342129042540148_1662995676_n.jpg\"\
@@ -35,96 +35,36 @@
 ,\"points\":100\
 ,\"currentPlayers\":21\
 },\
-{\"id\": 3, \"title\":\"50% Sandwich\", \"loc\":[49.273868, -123.111961]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
+{\"id\": 3, \"title\":\"The Irish Cupid\", \"loc\":[49.265639,-123.247046]\
+,\"name\":\"Fall in love with Ireland!\"\
+,\"desc1\":\"Have an Irish drink with your lady. Guinness, Kilkenny, \"\
+,\"desc2\":\"or an Irish Car Bomb.\"\
 ,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
+,\"headerImg\":\"http://starevents.com/wp-content/uploads/2012/12/hat.png\"\
 ,\"brandImg\":\"\"\
 ,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
 ,\"endDateTime\":\"2013-02-28T00:00:00.000Z\"\
 ,\"points\":0\
 ,\"currentPlayers\":0\
 },\
-{\"id\": 4, \"title\":\"Free Bubble Tea\", \"loc\":[49.283868, -123.121961]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
+{\"id\": 4, \"title\":\"Canadian Americano\", \"loc\":[49.263711,-123.227663]\
+,\"name\":\"Take a break at the boulevard\"\
+,\"desc1\":\"Sit down, relax, enjoy a drink with your date.\"\
 ,\"desc2\":\"\"\
 ,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
+,\"headerImg\":\"http://theboulevard.ca/topbar.jpg\"\
 ,\"brandImg\":\"\"\
 ,\"startDateTime\":\"2013-03-24T00:00:00.000Z\"\
 ,\"endDateTime\":\"2013-04-30T00:00:00.000Z\"\
 ,\"points\":0\
 ,\"currentPlayers\":0\
 },\
-{\"id\": 5, \"title\":\"Free Pop\", \"loc\":[49.273868, -123.126961]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
+{\"id\": 5, \"title\":\"Bike Trip\", \"loc\":[49.313246, -123.140648]\
+,\"name\":\"From Under the Bridge\"\
+,\"desc1\":\"Take a bike trip along Stanley Park Seawall\"\
+,\"desc2\":\"Pose a picture a picture of the Lions Gate from below the bridge!\"\
 ,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
-,\"brandImg\":\"\"\
-,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
-,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
-,\"points\":0\
-,\"currentPlayers\":0\
-},\
-{\"id\": 6, \"title\":\"Free Hotdog\", \"loc\":[49.278868, -123.101961]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
-,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
-,\"brandImg\":\"\"\
-,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
-,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
-,\"points\":0\
-,\"currentPlayers\":0\
-},\
-{\"id\": 7, \"title\":\"Free Cheese Burger\", \"loc\":[49.216338, -122.971009]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
-,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
-,\"brandImg\":\"\"\
-,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
-,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
-,\"points\":0\
-,\"currentPlayers\":0\
-},\
-{\"id\": 8, \"title\":\"Free Rice Noodle\", \"loc\":[49.206338, -122.981009]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
-,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
-,\"brandImg\":\"\"\
-,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
-,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
-,\"points\":0\
-,\"currentPlayers\":0\
-},\
-{\"id\": 9, \"title\":\"$2 off Quizno's\", \"loc\":[49.216338, -122.991009]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
-,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
-,\"brandImg\":\"\"\
-,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
-,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
-,\"points\":0\
-,\"currentPlayers\":0\
-},\
-{\"id\": 10, \"title\":\"Free Tacos\", \"loc\":[49.226338, -122.981009]\
-,\"name\":\"\"\
-,\"desc1\":\"\"\
-,\"desc2\":\"\"\
-,\"desc3\":\"\"\
-,\"headerImg\":\"\"\
+,\"headerImg\":\"http://knittingthecamino.files.wordpress.com/2009/03/lions-gate.jpg?w=300&h=225\"\
 ,\"brandImg\":\"\"\
 ,\"startDateTime\":\"2013-02-24T00:00:00.000Z\"\
 ,\"endDateTime\":\"2013-03-31T00:00:00.000Z\"\
